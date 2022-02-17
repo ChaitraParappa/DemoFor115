e3998ede029fe871017b55861922983e15be1182
Chaitra <chaitra.p321@gmail.com>
2022-02-17 09:20:58 +0530

    Program.cs of FlipCoin

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Please enter the value for number of flips");
+
+            //int flipCoin1 = Convert.ToInt32(Console.ReadLine());
+            //double value =FlipCoin.FlipTheCoin(flipCoin1,2);
+
+            //Console.WriteLine("Head Percentage : " + value);
+
+            SwitchDemo.DisplayMonth();
+
+
+        }
+    }
+}
